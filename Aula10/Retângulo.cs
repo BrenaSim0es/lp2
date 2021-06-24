d654f78270e7f66c397ff00ddfa384b62b323d56
eullarissa <83432642+eullarissa@users.noreply.github.com>
2021-06-23 23:06:19 -0300

    Update Retângulo.cs

--- Aula10/Retângulo.cs
+++ Aula10/Retângulo.cs
@@ -4,31 +4,8 @@ namespace  Aula10
 {
  classe  pública Retângulo : Figura 
 {
-     _raio duplo  privado ; // atributo
-    público duplo Raio // propriedade
-    {
-        // leitura do atributo correspondente
-        obter
-        {
-            return this._raio;
-        }
-        
-        // escrita do atributo correspondente
-        definir
-        {
-            if (valor <= 0)
-            {
-                Console.WriteLine ("Raio inválido");
-            }
-            senão
-            {
-                isso . _raio  =  valor ;
-                isso . AtualizarArea ();
-                isso . AtualizarPerimetro ();
-            }
-        }
-    }
-     Circulo público ( raio duplo  )
+     
+     Retangulo público ( )
     {
         // escrita utilizando método set da propriedade
         isso . Raio  =  raio ;
